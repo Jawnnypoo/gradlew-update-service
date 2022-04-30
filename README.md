eee90f6c1509d327ba195869f1cacf982931081c
John Carlson <jawnnypoo@gmail.com>
2022-04-29 22:51:40 -0500

    :100:

---
+++ README.md
@@ -0,0 +1,2 @@
+# gradlew-update-service
+Dead simple backend service for [gradlew-update](https://github.com/Jawnnypoo/gradlew-update)
---
+++ server.js
@@ -0,0 +1,35 @@
+const fastify = require('fastify')({ logger: true })
+const axios = require('axios')
+
+fastify.get('/', async (request, reply) => {
+    try {
+        const response = await axios({
+            method: 'get',
+            url: 'https://docs.gradle.org/current/userguide/userguide.html',
+            responseType: 'text'
+        })
+        const html = response.data.toString()
+        const startHtml = '<script type="text/javascript" defer>\n  window.siteDecorateVersion = "'
+        const startIndex = html.indexOf(startHtml)
+        const endIndex = html.indexOf('";', startIndex)
+        const value = html.substring(startIndex + startHtml.length, endIndex)
+        console.log(value);
+        reply.code(200)
+            .send(value)
+    } catch (error) {
+        console.error(error);
+        reply.code(500)
+            .send()
+    }
+})
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen(3000)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
